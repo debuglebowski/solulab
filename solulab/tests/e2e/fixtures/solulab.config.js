a3db4417e94f2fa90d96448599dbfe64a16052fc
debuglebowski <kh.hansson@gmail.com>
2025-07-24 21:24:09 +0200

    Remove e2e

--- solulab/tests/e2e/fixtures/solulab.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    labGlobs: ['*.lab.ts'],
-};
--- solulab/tests/e2e/helpers/index.ts
+++
@@ -1,5 +0,0 @@
-// Re-export all helper functions from their respective modules
-export { runCLI, type CLIResult } from './runCLI/index';
-export { createTestEnv } from './createTestEnv/index';
-export { setupFixtures } from './setupFixtures/index';
-export { getDatabaseStats } from './getDatabaseStats/index';
